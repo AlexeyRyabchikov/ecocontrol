f68559ea4222d71e27f4b84a2bafb00f1993d4e2
aleksey.ryabchikov <al.ryabchikov@gmail.com>
2021-11-12 20:22:06 +0400

    Add claim model

--- db/migrate/20211112144700_create_companies.rb
+++ db/migrate/20211112144700_create_companies.rb
@@ -1,6 +1,6 @@
 class CreateCompanies < ActiveRecord::Migration[6.1]
   def change
-    create_table :users do |t|
+    create_table :companies do |t|
       t.string :name
       t.text :description
       t.string :email
