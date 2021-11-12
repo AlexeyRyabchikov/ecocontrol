27a9ca409359c781f3b75994cb5fbda4f9317d2a
Александр Герасимов <a.a.gerasimov@rt.ru>
2021-11-13 03:01:22 +0400

    Comment authorization

--- app/controllers/web/welcome_controller.rb
+++ app/controllers/web/welcome_controller.rb
@@ -1,5 +1,5 @@
 class Web::WelcomeController < Web::ApplicationController
-  skip_after_action :verify_authorized, only: [:index]
-  
+  # skip_after_action :verify_authorized, only: [:index]
+
   def index; end
 end
