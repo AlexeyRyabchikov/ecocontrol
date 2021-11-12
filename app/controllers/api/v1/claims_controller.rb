338cee132cc7501a04004dcfafda1ba346725a93
aleksey.ryabchikov <al.ryabchikov@gmail.com>
2021-11-12 22:58:36 +0400

    Add claims_controller.rb

---
+++ app/controllers/api/v1/claims_controller.rb
@@ -0,0 +1,51 @@
+class Api::V1::ClaimsController < Api::V1::ApplicationController
+
+  def new
+    claim = Claim.new
+  end
+
+  def create
+    claim = Claim.new(claim_attrs)
+
+    if claim.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
+  def show
+    claim = Claim.find(params[:id])
+    render json: claim
+  end
+
+  def index
+    claims = Claim.all
+    render json: claims
+  end
+
+  def edit
+    claim = Claim.find(params[:id])
+  end
+
+  def update
+    claim = Claim.find(params[:id])
+
+    if claim.update(claim_attrs)
+      redirect_to action: :index
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    claim = Claim.find(params[:id])
+    claim.destroy
+  end
+
+  private
+
+  def claim_attrs
+    params.require(:claim).permit(:title, :description)
+  end
+end
