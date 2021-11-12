9e434f0bba5d8051cab01627f31e207549b07dcc
Alexandr Savin <aleksandr.vl.savin@rt.ru>
2021-11-12 22:35:35 +0400

    okved- dangerous code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_12_162319) do
+ActiveRecord::Schema.define(version: 2021_11_12_183337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2021_11_12_162319) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "dangerous", default: false
     t.index ["code"], name: "okved_code", unique: true
   end
 
