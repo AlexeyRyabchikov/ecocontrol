1413f91fd4b725b6ce042c7f78a0c014340df2fc
Alexandr Savin <aleksandr.vl.savin@rt.ru>
2021-11-13 21:30:14 +0400

    sort companies by dangerous status

--- app/controllers/web/companies_controller.rb
+++ app/controllers/web/companies_controller.rb
@@ -1,6 +1,15 @@
 class Web::CompaniesController < Web::ApplicationController
   def index
-    @companies = Company.all
+    @companies = Company.all.sort_by do |company|
+      case
+      when !company.nvos_checked && company.okveds.first.dangerous && company.clame_counter > 0
+        0
+      when !company.nvos_checked && (company.okveds.first.dangerous || company.clame_counter > 0)
+        1
+      else
+        2
+      end
+    end
     render :index
   end
 end
