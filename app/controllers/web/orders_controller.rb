5f96a86c92260596ccc448060869b364c97dcffd
Александр Герасимов <a.a.gerasimov@rt.ru>
2021-11-13 16:20:06 +0400

    Add orders list

--- app/controllers/web/orders_controller.rb
+++ app/controllers/web/orders_controller.rb
@@ -1,6 +1,10 @@
 class Web::OrdersController < Web::ApplicationController
   def search_companies; end
 
+  def index
+    @orders = Order.joins(:companies).all.order('date desc').group('orders.id').select('orders.*', 'COUNT(companies.id) as companies_count')
+  end
+
   def new
     @order = Order.new
     @companies = Company.where(nvos_checked: false)
